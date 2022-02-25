111ebbc12b9b7781caca00bfa5c2c322e735a4a4
Muhammad Farid Zaki <muhammadfarid.zaki@gmail.com>
2022-02-25 03:25:28 +0000

    Initial commit

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Component} from 'react/cjs/react.production.min';
 import main from "../screens/main"
-import LoginScreen from "../screens/Login Screen"
 
 const Stack = createNativeStackNavigator();
 export default class App extends Component {
@@ -11,11 +10,6 @@ export default class App extends Component {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            options={{headerShown: false}}
-          />
         <Stack.Screen
             name="main"
             component={main}
