1dd5bc5483e8dc59ce73363561bdced0595b4cd5
Muhammad Farid Zaki <muhammadfarid.zaki@gmail.com>
2022-03-15 10:50:55 +0700

    adding comment feature

--- src/screens/detailsScreen/index.js
+++ src/screens/detailsScreen/index.js
@@ -1,21 +1,65 @@
-import {Text, StyleSheet, View, Image, ScrollView} from 'react-native';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native';
 import React, {Component} from 'react';
 import CText from '../../components/atoms/CText';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 export default class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
+      comment: '',
+      dataComments: [],
     };
   }
-  componentDidMount() {
+  async componentDidMount() {
     const {params} = this.props.route;
-    this.setState({data: params});
-  }
-  render() {
     const {data} = this.state;
-    console.log(data);
+    this.setState({data: params});
+    this.mounted = true;
+    await firestore()
+      .collection('Comments')
+      .doc(`${params.time}`)
+      .onSnapshot(x => {
+        if (x.data() != null) {
+          let cup = x.data().comments;
+          if (cup) {
+            let sorted = cup.flat().sort((a, b) => a.time - b.time);
+            this.mounted == true && this.setState({dataComments: sorted});
+          }
+        }
+      });
+  }
+  _postcomment = async () => {
+    const {data, comment} = this.state;
+    await firestore()
+      .collection('Comments')
+      .doc(`${data.time}`)
+      .set(
+        {
+          comments: firestore.FieldValue.arrayUnion({
+            displayName: auth().currentUser.displayName,
+            photoURL: auth().currentUser.photoURL,
+            comment: comment,
+            time: new Date(),
+            uid:auth().currentUser.uid
+          }),
+        },
+        {merge: true},
+      );this.setState({comment:''})
+  };
+  render() {
+    const {data, comment, dataComments} = this.state;
     return (
       <ScrollView style={styles.container}>
         <View style={{margin: 10, borderWidth: 5, borderColor: 'black'}}>
@@ -65,15 +109,64 @@ export default class Index extends Component {
               <Text>Nama yang upload</Text>
             </View>
           </View>
-          <View
-            style={{
-              padding: 5,
-              flexDirection: 'row',
-              backgroundColor: 'white',
-              marginBottom: 3,
-            }}>
-            <CText>Comments</CText>
-            <View></View>
+          <View style={{backgroundColor: 'white'}}>
+            <View
+              style={{
+                padding: 5,
+                backgroundColor: 'white',
+                marginBottom: 3,
+              }}>
+              <CText>Comments</CText>
+              <View
+                style={{borderWidth: 1, borderColor: 'black', borderRadius: 1}}
+                >
+                {dataComments &&
+                  dataComments.map((x, i) => {
+                    return (
+                      <View
+                        key={i}
+                        style={x.uid!=auth().currentUser.uid?{
+                          padding:5,
+                          width:'50%',
+                          borderWidth: 1,
+                          borderColor: 'black',
+                          position:'relative',
+                          left:0,borderBottomEndRadius:25,borderBottomStartRadius:25,borderTopEndRadius:25
+                        }:{padding:5,
+                          width:'50%',
+                          borderWidth: 1,
+                          borderColor: 'black',
+                          position:'relative',
+                          left:'50%',borderBottomEndRadius:25,borderBottomStartRadius:25,borderTopStartRadius:25}}>
+                        <Text style={{...styles.textcolor,fontWeight:'bold'}}>{x.displayName}</Text>
+                        <Text style={styles.textcolor}>{x.comment}</Text>
+                      </View>
+                    );
+                  })}
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 15,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <TextInput
+                placeholder="comment..."
+                placeholderTextColor={'grey'}
+                value={comment}
+                color={'black'}
+                onChangeText={value => this.setState({comment: value})}
+                style={{
+                  borderBottomWidth: 1,
+                  borderColor: 'black',
+                  alignItems: 'center',
+                  width: '60%',
+                }}
+              />
+              <Button title="comment" onPress={() => this._postcomment()} />
+            </View>
           </View>
         </View>
       </ScrollView>
